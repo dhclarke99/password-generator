9c06deec48a0d0d307571e6ac4be760b5f9fa9be
dhclarke99 <dhclarke99@gmail.com>
2023-03-23 18:42:06 -0400

    created variable and prompt for password length

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var passLength = window.prompt("Choose a password length (Must be between 8 and 128 characters):");
+  if (passLength < 8 || passLength > 128) {
+    window.alert("Invalid Password Length");
+}
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
